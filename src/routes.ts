07c8aa089bc7472df5517315cf3f520809af2b92
Soheil Najmabadi Kia <sohnajm@gmail.com>
2023-07-29 22:49:28 -0230

    w3cwebsocket

--- src/routes.ts
+++ src/routes.ts
@@ -30,43 +30,5 @@ export const Routes = [
         action: "addOnlineGame",
     } 
 
-    // {
-    //     //Get all users--
-    //     method: "get",
-    //     route: "/api/users",
-    //     middlewares: [checkRole(["ADMIN"])],
-    //     controller: UserController,
-
-    //     action: "all",
-    // }, {
-    //     // Get one user-- 
-    //     method: "get",
-    //     route: "/api/users/:id([0-9]+)",
-    //     middlewares: [checkRole(["ADMIN"])],
-    //     controller: UserController,
-    //     action: "one",
-    // }, {
-    //     //Edit one user--
-    //     method: "put", // or patch
-    //     route: "/api/users/:id([0-9]+)",
-    //     middlewares: [checkRole(["ADMIN"])],
-    //     controller: UserController,
-    //     action: "edit",
-    // }, {
-    //     //Delete one user--
-    //     method: "delete",
-    //     route: "/api/users/:id([0-9]+)",
-    //     middlewares: [checkRole(["ADMIN"])],
-    //     controller: UserController,
-    //     action: "remove",
-    // }, {
-    //     //Get online games
-    //     method: "get",
-    //     route: "/api/games",
-    //     middlewares: [checkRole(["ADMIN"])],
-    //     controller: UserController,
-    //     action: "remove",
-    // }, 
-
 ];
 
